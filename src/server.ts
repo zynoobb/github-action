836bd7399c1933af4434aa77caec757c02155447
zynoobb <117588482+zynoobb@users.noreply.github.com>
2023-05-26 22:06:20 +0900

    Test : CD테스트2

--- src/server.ts
+++ src/server.ts
@@ -4,4 +4,5 @@ app.listen(3000, () => {
   console.log("😎😎 server is running 😎😎");
 });
 
-// github-action 테스트를 위해 커밋을 남기는 주석입니다.
+// github-action 테스트를 위해 커밋을 남기는 주석입니다. 
+// 2. secret zone key 수정 후 다시 남기는 주석.
